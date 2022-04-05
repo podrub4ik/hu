52128dab8083b4060dbea888e091b1ccd215a9c8
podrub4ik <103022069+podrub4ik@users.noreply.github.com>
2022-04-05 09:40:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("привет андрей");
+        }
+    }
+}
